5fd23529b865134c93bbc0e24fe0338bed40b342
edugerez <selmaredugerez@gmail.com>
2023-02-13 01:02:06 -0300

    fixing if to BOM

--- lessson5/scripts/bom.js
+++ lessson5/scripts/bom.js
@@ -1,23 +1,25 @@
 
 
 const button= document.getElementById("add-chapter");
+const input =document.querySelector("#favchap");
 
 const list= document.querySelector("#list")
 
 
 button.addEventListener('click', function (){
+   if(input.value !==''){
 
    let li=document.createElement("li");
    const liDelete = document.createElement("button");
    liDelete.innerHTML="X"
    liDelete.style.color="red";
-   let input=document.querySelector("#favchap").value;
-   li.innerText=(input);
+   const book=input.value;
+   li.innerText=(book);
    list.append(li);
-   let empty=document.getElementById("favchap").value=" ";
-   let focus=document.getElementById("favchap")
+   const empty=input.value="";
+   const focus=input
    focus.focus()
    li.append(liDelete)
-   liDelete.addEventListener("click", ()=>li.remove()) 
+   liDelete.addEventListener("click", ()=>li.remove())};
 })
 
