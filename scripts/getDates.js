7b51ac98767d1c5a36f6495d576386fa905da94c
Eduardo <113535626+edugerez@users.noreply.github.com>
2024-05-08 23:03:39 -0300

    humburger menu

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -1,6 +1,20 @@
 // Select the HTML element to manipulate
 const date = document.querySelector("#date");
 const message = document.querySelector("#emessage");
+const nav = document.getElementById("nav");
+const closeButton = document.getElementById("close");
+const openButton = document.getElementById("open");
+
+openButton.addEventListener("click", () => {
+    nav.classList.add("nav");
+})
+
+closeButton.addEventListener("click", () => {
+    nav.classList.remove("nav");
+})
+
+
+
 
 //  method with options
 try {
@@ -18,3 +32,5 @@ catch (e) {
 
 
 document.getElementById("update").innerHTML = new Date(document.lastModified);
+
+
